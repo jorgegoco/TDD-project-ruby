e90fcf7942a1d457423ccda2a06bdcc1cbf0063a
jorgegoco <jorgegoco70@gmail.com>
2023-02-16 12:03:08 +0100

    Test factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,4 +2,19 @@ require 'rspec'
 require_relative '../solver'
 
 describe Solver do
+  subject { Solver.new }
+
+  describe '#factorial' do
+    it 'Test with number > 0' do
+      expect(subject.factorial(5)).to eq 120
+    end
+
+    it 'Test with number = 0' do
+      expect(subject.factorial(0)).to eq 1
+    end
+
+    it 'Test with number < 0' do
+      expect { raise "not allowed for negatives values" }.to raise_error("not allowed for negatives values")
+    end
+  end
 end
